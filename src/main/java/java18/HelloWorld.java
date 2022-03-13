ac5d84d22179eb22347f81adb766a482480690d6
Frank W. Rahn <frank@frank-rahn.de>
2022-03-13 20:41:16 +0100

    Die Änderungen in Java 18 dokumentiert

---
+++ src/main/java/java18/HelloWorld.java
@@ -0,0 +1,18 @@
+package java18;
+
+/**
+ * ...
+ *
+ * {@snippet :
+ * System.out.println("Hello World!");
+ * }
+ *
+ * {@snippet file="java18/HelloWorld.java" region="output"}
+ */
+public class HelloWorld {
+  public static void main(String[] args){
+    // @start region="output"
+    System.out.println("Hello World!");
+    // @end
+  }
+}
