9a470c3b4f66e983b2c1c8c3f1af8f93119b2399
Frank W. Rahn <frank@frank-rahn.de>
2022-10-01 19:54:32 +0200

    records für Java 16 erweitert

--- src/main/java/java16/Records.java
+++ src/main/java/java16/Records.java
@@ -6,9 +6,25 @@ public class Records {
 
   public record NamedTuple<T>(String name, T... values) {}
 
+  public record Range(int low, int high) {
+    public Range() {
+      this(Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public Range {
+      if (low > high) {
+        throw  new IllegalArgumentException("low greater high");
+      }
+    }
+
+    public int length() {
+      return high - low;
+    }
+  }
+
   public static void main(String[] args) {
-    Data data = new Data("Test", 5711);
-    Data data2 = new Data("Test", 5711);
+    var data = new Data("Test", 5711);
+    var data2 = new Data("Test", 5711);
 
     System.out.println(data.a());
     System.out.println(data.b());
@@ -18,5 +34,10 @@ public class Records {
 
     var tuple = new NamedTuple<>("Geld", 0.5, 3.14);
     System.out.println(tuple);
+
+    var range = new Range();
+    System.out.println(range);
+    System.out.println(range.length());
+    new Range(2, 1);
   }
 }
